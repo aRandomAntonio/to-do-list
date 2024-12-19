565ff5f529cba6d6660983d282a584bebec1f974
aRandomAntonio <antoniocnunes2000@gmail.com>
2024-12-18 22:54:32 -0300

    trying to fix again

--- src/script.ts
+++ src/script.ts
@@ -41,7 +41,7 @@ function add(): void {
         texts.push(text);
 
         let img = document.createElement("img") as HTMLImageElement;
-        img.src = "/public/img/3dots.png";
+        img.src = "public/img/3dots.png";
         img.id = `image${count}`;
         img.className = "img";
         img.addEventListener("click", (event) => {
@@ -123,7 +123,7 @@ function open(target: HTMLElement): void {
                 texts[i] = newText;
 
                 let newImg = document.createElement("img");
-                newImg.src = "/public/img/3dots.img";
+                newImg.src = "public/img/3dots.img";
                 newImg.id = `image${count}`;
                 newImg.className = "img";
                 newImg.addEventListener("click", (event) => {
